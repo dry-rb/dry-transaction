7049a880a5a0a90a4ae5f19edca8043f22ffc322
GustavoCaso <gustavocaso@gmail.com>
2017-05-18 08:01:50 +0200

    Add back append, prepend, remove and insert

--- lib/dry/transaction/instance_methods.rb
+++ lib/dry/transaction/instance_methods.rb
@@ -8,7 +8,8 @@ module Dry
       def initialize(matcher: ResultMatcher, steps: (self.class.steps), **operations)
         @steps = steps.map { |step|
           operation = methods.include?(step.step_name) ? method(step.step_name) : operations[step.step_name]
-          step.with(operation: operation)
+          step = step.with(operation: operation) if operation
+          step
         }
         @operations = operations
         @matcher = matcher
@@ -55,6 +56,38 @@ module Dry
         self.class.new(matcher: matcher, steps: new_steps, **operations)
       end
 
+      def prepend(other = nil, **options, &block)
+        other = accept_or_build_transaction(other, **options, &block)
+
+        self.class.new(matcher: matcher, steps: other.steps + steps)
+      end
+
+      def append(other = nil, **options, &block)
+        other = accept_or_build_transaction(other, **options, &block)
+
+        self.class.new(matcher: matcher, steps: steps + other.steps)
+      end
+
+      def insert(other = nil, before: nil, after: nil, **options, &block)
+        insertion_step = before || after
+        match_insertion_step = proc { |step| step.step_name == insertion_step }
+
+        unless steps.any?(&match_insertion_step)
+          raise ArgumentError, "+#{insertion_step}+ is not a valid step name"
+        end
+
+        other = accept_or_build_transaction(other, **options, &block)
+        index = steps.index(&match_insertion_step) + (after ? 1 : 0)
+
+        self.class.new(matcher: matcher, steps: steps.dup.insert(index, *other.steps))
+      end
+
+      def remove(*steps_to_remove)
+        new_steps = steps.reject { |step| steps_to_remove.include?(step.step_name) }
+        self.class.new(matcher: matcher, steps: new_steps)
+      end
+
+
       private
 
       def respond_to_missing?(name, _include_private = false)
@@ -90,6 +123,22 @@ module Dry
           if num_args_required > num_args_supplied
             raise ArgumentError, "not enough arguments supplied for step +#{step.step_name}+"
           end
+        end
+      end
+
+      def accept_or_build_transaction(other_transaction = nil, **options, &block)
+        unless other_transaction || block
+          raise ArgumentError, "a transaction must be provided or defined in a block"
+        end
+
+        if other_transaction
+          other_transaction
+        else
+          Class.new do
+            include Dry.Transaction(container: options[:container])
+
+            instance_eval(&block)
+          end.new
         end
       end
     end
