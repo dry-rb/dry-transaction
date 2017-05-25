af2298e89c754d6539f850ff7db591e41a64bbe1
Tim Riley <tim@openmonkey.com>
2017-05-25 10:53:45 +1000

    Stop supporting blocks for steps
    Now that we can wrap them in local methods, we shouldn’t need this

--- lib/dry/transaction/step.rb
+++ lib/dry/transaction/step.rb
@@ -16,16 +16,14 @@ module Dry
       attr_reader :operation_name
       attr_reader :operation
       attr_reader :options
-      attr_reader :block
       attr_reader :call_args
 
-      def initialize(step_adapter, step_name, operation_name, operation, options, call_args = [], &block)
+      def initialize(step_adapter, step_name, operation_name, operation, options, call_args = [])
         @step_adapter = step_adapter
         @step_name = step_name
         @operation_name = operation_name
         @operation = operation
         @options = options
-        @block = block
         @call_args = call_args
       end
 
@@ -41,13 +39,12 @@ module Dry
           new_operation,
           options,
           new_call_args,
-          &block
         )
       end
 
       def call(input)
         args = [input] + Array(call_args)
-        result = step_adapter.call(self, *args, &block)
+        result = step_adapter.call(self, *args)
 
         result.fmap { |value|
           broadcast :"#{step_name}_success", value
